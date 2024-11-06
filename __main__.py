e617a0f8fccd40f82d41894549cab95f6c2036c2
Brian Shaw <brian@beclicking.com>
2024-11-05 23:17:47 -0600

    test exit

--- __main__.py
+++ __main__.py
@@ -157,6 +157,7 @@ async def main():
 
 
 async def cleanup():
+    global rpiButtonsLeds, lights
     print('Cleaning up...')
     if rpiButtonsLeds:
         print('Cleaning up RpiButtonsLeds...')
