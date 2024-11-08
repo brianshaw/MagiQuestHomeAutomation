3791efdf480a33bd0c43675263314238ec53f1ca
Brian Shaw <brian@beclicking.com>
2024-11-08 15:14:41 -0600

    test s

--- __main__.py
+++ __main__.py
@@ -96,8 +96,8 @@ async def main():
     #           t0 = -1
     #           t1 = -1
     if args['rpi']:
-      app='sudo aplay'
-      # app='mpg321'
+      # app='sudo aplay'
+      app='mpg321'
     else:
       app='afplay'
     if args['test']:
