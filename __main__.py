413745d16553467b2be3eb9bee7a39d06bf3abf7
Brian Shaw <brian@beclicking.com>
2024-11-08 15:09:54 -0600

    test s

--- __main__.py
+++ __main__.py
@@ -96,7 +96,7 @@ async def main():
     #           t0 = -1
     #           t1 = -1
     if args['rpi']:
-      app='MPG321'
+      app='mpg321'
     else:
       app='afplay'
     if args['test']:
